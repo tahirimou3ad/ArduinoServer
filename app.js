8940ca8c60895bbcdf911c76ea0b6f57b6ff8a62
mouadDahhoumi <moauddahhoumi@gmail.com>
2022-01-10 15:35:27 +0100

    3rd commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 8000;
 const router = require('./controllers/router');
 const bodyParser = require('body-parser')
@@ -12,6 +15,40 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+  /*var err = new Error('Not Found');
+   err.status = 404;
+   next(err);*/
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  // Pass to next layer of middleware
+  next();
+});
+
+const io = require('socket.io')(3031, {
+  cors: {
+      origin: 'http://localhost:3000',
+  }
+})
+
+app.io = io;
+
+io.on("connection", socket => {
+  //SerialPort
+  const id = 12;
+  socket.emit("send-id", id);
+})
+
 app.use('/', router);
 app.use(express.static('public'));
 
@@ -19,7 +56,7 @@ app.listen(PORT, () => {
     console.log(`The server is now listening on port ${PORT} ... `);
 });
 
-const SerialPort = require('serialport');
+const SerialPort = require('serialport'); 
 const ReadLine = SerialPort.parsers.Readline;
 
 const port = new SerialPort("COM4", {
@@ -35,8 +72,11 @@ parser.on('data', async (data) => {
   if (data){
     const temperature = parseInt(data);
     console.log(temperature);
-    const notification = new Notification({notificationType: "temperature", temperature})
+    const notification = new Notification({notificationType: "temperature", temperature});
+    const description = `Temperature actuelle: ${temperature} `
+    req.app.io.emit('popup', {title: "Porte Ouverte !", description});
     
+    // Send message to the UI
     try {
         await notification.save();
 
--- controllers/router.js
+++ controllers/router.js
@@ -1,63 +1,103 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Product = require('../models/product');
-const Notification = require('../models/notification');
-const cron = require('node-cron');
+const Product = require("../models/product");
+const Notification = require("../models/notification");
+const cron = require("node-cron");
+const io = require('socket.io')
 
-router.get('/', (req, res) => {
-    res.send("Hello from server !");
+router.get("/", (req, res) => {
+  res.send("Hello from server !");
 });
 
 // request body should be like this: {name, dateExp}
-router.post('/products', async (req, res) => {
-    const product = new Product(req.body);
+router.post("/products", async (req, res) => {
+  const product = new Product(req.body);
 
-    try {
-        await product.save();
-        const alertDate = new Date(req.body.dateExp);
-        const onloadDate = Date.now();
-        const tOut = alertDate - onloadDate;
+  try {
+    await product.save();
+    const alertDate = new Date(req.body.dateExp);
+    const onloadDate = Date.now();
+    const tOut = alertDate - onloadDate;
 
-        if (tOut > 0) {
-            setTimeout(async ()=>{ 
-                const notification = new Notification({notificationType: 'expiration', product});
-                await notification.save();
-             }, tOut )
-        } else {
-            const notification = new Notification({notificationType: 'expiration', product});
-            await notification.save();
-        }
-        res.status(201).send({ product })
-    } catch (e) {
-        res.status(400).send(e)
+    if (tOut > 0) {
+      setTimeout(async () => {
+        const notification = new Notification({
+          notificationType: "expiration",
+          product,
+        });
+        await notification.save();
+        // Send popup to UI
+        const { nom, dateExp } = req.body;
+        const description = `Le produit ${nom} est périmé. Date d'expiration (${dateExp}) `
+        req.app.io.emit('popup', {title: "Alerte Expiration", description});
+      }, tOut);
+    } else {
+      const notification = new Notification({
+        notificationType: "expiration",
+        product,
+      });
+      await notification.save();
+      const { nom, dateExp } = req.body;
+      const description = `Le produit ${nom} est périmé. Date d'expiration (${dateExp}) `
+      req.app.io.emit('popup', {title: "Alerte Expiration", description});
+
     }
-})
+    res.status(201).send({ product });
+  } catch (e) {
+    console.log(e)
+    res.status(400).send(e);
+  }
+});
 
 // To get all products
-router.get('/products', async (req, res) => {
-    try {
-        const produits = await Product.find({}).lean();
-        res.status(201).send(produits);
-    } catch (e) {
-        res.status(500).send();
-    }
-})
+router.get("/products", async (req, res) => {
+  try {
+    const produits = await Product.find({}).lean(); 
+    res.status(201).send(produits);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-router.get('/products/:id', async (req,res) => {
-    const _id = req.params.id
+router.delete("/products/:id", async (req, res) => {
     try {
-        const product = await Product.findById(_id);
-         
-        if (!product) {
-            return res.status(404).send()
-        }
+      const product = await Product.findOneAndDelete({
+        _id: req.params.id,
+      });
 
-        console.log(product)
-        res.send(product)
+      const notification = await Notification.findOneAndDelete({
+        product: req.params.id,
+        notificationType: "expiration"
+      });
+  
+      if (!product) {
+        res.status(404).send();
+      }
+      console.log(product)
+      res.send(product);
+      res.send(notification);
+
     } catch (e) {
-        res.status(500).send(e)
+      res.status(500).send();
     }
-})
+  });
+
+router.get("/products/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const product = await Product.findById(_id);
+
+    if (!product) {
+      return res.status(404).send();
+    }
+
+    console.log(product);
+    res.send(product);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 
 
 // Method: POST
@@ -87,25 +127,60 @@ router.get('/products/:id', async (req,res) => {
 // })
 
 // To get all temperature notification
-router.get('/notifications/temperature', async (req, res) => {
-    try {
-        const notifications = await Notification.find({notificationType: 'temperature'}).lean();
-        res.status(201).send({ notifications })
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+router.get("/notifications/temperature", async (req, res) => {
+  try {
+    const notifications = await Notification.find({
+      notificationType: "temperature",
+    }).lean();
+    res.status(201).send(notifications);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 // To get all expiration notifications
-router.get('/notifications/expiration', async (req, res) => {
-    try {
-        const notifications = await Notification.find({notificationType: 'expiration'}).populate('product').lean();
-        res.status(201).send({ notifications })
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+router.get("/notifications/expiration", async (req, res) => {
+  try {
+    const notifications = await Notification.find({
+      notificationType: "expiration",
+    })
+      .populate("product")
+      .lean();
+    console.log(notifications);
+    res.status(201).send(notifications);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
+// To get all notifications
+router.get("/notifications/", async (req, res) => {
+  try {
+    const notifications = await Notification.find({})
+      .populate("product")
+      .lean();
+    res.status(201).send(notifications);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete("/notifications/:id", async (req, res) => {
+    try {
+      const notification = await Notification.findOneAndDelete({
+        _id: req.params.id,
+      });
+ 
+      if (!notification) {
+        res.status(404).send();
+      }
+
+      res.send(notification);
+
+    } catch (e) {
+      res.status(500).send();
+    }
+  });
 
 
 module.exports = router;
